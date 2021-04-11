e7735428e0e8b0ebd7c11cdb26efbaf387a7a4bc
hippy <hippy2981@gmail.com>
2021-04-11 15:48:18 -0700

    random: Add random impl

---
+++ src/rm.Random2/RandomFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace rm.Random2
+{
+	/// <note>
+	/// see https://csharpindepth.com/Articles/Random
+	/// </note>
+	public static class RandomFactory
+	{
+		private static readonly Random _random = new Random();
+		private static readonly RandomLocked _randomLocked = new RandomLocked(_random);
+		private static readonly RNGCryptoServiceProvider _rngCrypto = new RNGCryptoServiceProvider();
+		private static readonly RandomPerThread _randomPerThread = new RandomPerThread(_rngCrypto);
+
+		public static Random GetRandom()
+		{
+			return _random;
+		}
+
+		public static RandomLocked GetRandomLocked()
+		{
+			return _randomLocked;
+		}
+
+		public static RandomPerThread GetRandomLockedPerThread()
+		{
+			return _randomPerThread;
+		}
+	}
+}
---
+++ src/rm.Random2/RandomLocked.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace rm.Random2
+{
+	/// <note>
+	/// see https://devblogs.microsoft.com/pfxteam/getting-random-numbers-in-a-thread-safe-way/
+	/// </note>
+	public class RandomLocked
+	{
+		private readonly Random _random;
+
+		internal RandomLocked(Random random)
+		{
+			_random = random
+				?? throw new ArgumentNullException(nameof(random));
+		}
+
+		public int Next(int minValue = 0, int maxValue = int.MaxValue)
+		{
+			lock (_random)
+			{
+				return _random.Next(minValue, maxValue);
+			}
+		}
+	}
+}
