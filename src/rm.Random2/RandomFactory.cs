9983cc3c7bda7082ec3e88511bd6c888c4de5a0a
hippy <hippy2981@gmail.com>
2021-04-11 15:48:18 -0700

    random: Add random impl

---
+++ src/rm.Random2/RandomFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace rm.Random2
+{
+	/// <note>
+	/// see https://csharpindepth.com/Articles/Random
+	/// </note>
+	public static class RandomFactory
+	{
+		private static readonly Random _random = new Random();
+		private static readonly RandomLocked _randomLocked = new RandomLocked();
+		private static readonly RandomPerThread _randomPerThread = new RandomPerThread();
+
+		public static Random GetRandom()
+		{
+			return _random;
+		}
+
+		public static RandomLocked GetRandomLocked()
+		{
+			return _randomLocked;
+		}
+
+		public static RandomPerThread GetRandomPerThread()
+		{
+			return _randomPerThread;
+		}
+	}
+}
