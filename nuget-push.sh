90e66d52f9f960c345d410cb7ad3ffdb02266155
hippy <hippy2981@gmail.com>
2021-04-11 17:21:23 -0700

    nuget: Nugetize (pack, push, delete)

---
+++ nuget-push.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#usage: nuget-push <version>
+
+if [[ "$1" == "-h" ]]; then print-file-comments "$0"; exit; fi
+
+version="$1"
+if [[ -z "$version" ]]; then
+	echo 1>&2 "fatal: version required"
+	exec print-file-comments "$0"
+	exit 1
+fi
+
+tag="nuget-$version"
+
+dotnet nuget push .nupkg/rm.Random2."$version".nupkg \
+	-k $(< ~/dump/.nuget.apikey) \
+	-s https://api.nuget.org/v3/index.json \
+	&& git push $(git remote) "$tag"
