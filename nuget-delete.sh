acb169f4b74fae69721c2a4b32aa03a3a78555ca
hippy <hippy2981@gmail.com>
2021-04-11 17:21:23 -0700

    nuget: Nugetize (pack, push, delete)

---
+++ nuget-delete.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#usage: nuget-delete <version>
+
+if [[ "$1" == "-h" ]]; then print-file-comments "$0"; exit; fi
+
+version="$1"
+if [[ -z "$version" ]]; then
+	echo 1>&2 "fatal: version required"
+	exec print-file-comments "$0"
+	exit 1
+fi
+
+dotnet nuget delete rm.Random2 "$version" \
+	-k $(< ~/dump/.nuget.apikey) \
+	-s https://api.nuget.org/v3/index.json \
+	--non-interactive
---
+++ nuget-pack.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#usage: nuget-pack <version>
+
+if [[ "$1" == "-h" ]]; then print-file-comments "$0"; exit; fi
+
+version="$1"
+if [[ -z "$version" ]]; then
+	echo 1>&2 "fatal: version required"
+	exec print-file-comments "$0"
+	exit 1
+fi
+
+tag="nuget-$version"
+commit_hash=$(git rev-parse @)
+metadata="$commit_hash"
+
+dotnet pack src/rm.Random2/rm.Random2.csproj \
+	-c Release \
+	--include-symbols //p:SymbolPackageFormat=snupkg \
+	-o .nupkg/ \
+	//p:PackageVersion="$version+$metadata" \
+	//p:PackageReleaseNotes="tag: $tag" \
+	&& git tag "$tag" -m "Create nuget tag $tag"
