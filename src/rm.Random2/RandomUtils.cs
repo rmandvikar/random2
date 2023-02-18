3eaab32e4059e2c924939605dbe8c4a9cdc6f566
hippy <hippy2981@gmail.com>
2023-02-18 12:46:17 -0800

    random: Gen randoms using global rng's seed

--- src/rm.Random2/RandomUtils.cs
+++ src/rm.Random2/RandomUtils.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Security.Cryptography;
 
 namespace rm.Random2;
 
 public static class RandomUtils
 {
-#if !NET6_0_OR_GREATER
 	/// <summary>
 	/// Random number generator used to generate seeds,
 	/// which are then used to create new random number
 	/// generators on a per-thread basis.
 	/// </summary>
-	private static readonly RNGCryptoServiceProvider rngCrypto = new();
-#endif
+	private static readonly Random globalRandom = new();
+	private static readonly object locker = new();
 
 	/// <summary>
 	/// Creates a new instance of Random. The seed is derived
@@ -21,14 +19,9 @@ public static class RandomUtils
 	/// </summary>
 	public static Random NewRandom()
 	{
-		const int bytesCount = 4;
-#if NET6_0_OR_GREATER
-		byte[] buffer = RandomNumberGenerator.GetBytes(bytesCount);
-#else
-		byte[] buffer = new byte[bytesCount];
-		rngCrypto.GetBytes(buffer);
-#endif
-		var seed = BitConverter.ToInt32(buffer, 0);
-		return new Random(seed);
+		lock (locker)
+		{
+			return new Random(globalRandom.Next());
+		}
 	}
 }
