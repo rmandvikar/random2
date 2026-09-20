a94fd4669f906a33fba996d480f231272e4ae084
hippy <hippy2981@gmail.com>
2026-09-19 19:12:50 -0700

    Revert "nuget: Rename name to rmandvikar.*"
    This reverts commit 4f2607b3767e54e031c70fa0e9842c4321638d0b.

--- nuget-delete.sh
+++ nuget-delete.sh
@@ -11,7 +11,7 @@ if [[ -z "$version" ]]; then
 	exit 1
 fi
 
-dotnet nuget delete rmandvikar.Random2 "$version" \
+dotnet nuget delete rm.Random2 "$version" \
 	-k $(< ~/dump/.nuget.apikey) \
 	-s https://api.nuget.org/v3/index.json \
 	--non-interactive
--- nuget-push.sh
+++ nuget-push.sh
@@ -13,7 +13,7 @@ fi
 
 tag="v$version"
 
-dotnet nuget push .nupkg/rmandvikar.Random2."$version".nupkg \
+dotnet nuget push .nupkg/rm.Random2."$version".nupkg \
 	-k $(< ~/dump/.nuget.apikey) \
 	-s https://api.nuget.org/v3/index.json \
 	&& git push $(git remote) "$tag"
